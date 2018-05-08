52d6d8895b558a05b74c85b486d4526058c92f86
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2018-05-08 11:08:15 +0600

    Initial commit

---
+++ generators.py
@@ -0,0 +1,18 @@
+def fibonacci():
+    a, b = 0, 1
+
+    while True:
+        c = a + b
+        yield c
+        a, b = b, c
+
+
+def main():
+    fib = fibonacci()
+
+    for n in range(0, 100):
+        print(next(fib))
+
+
+if __name__ == '__main__':
+    main()
